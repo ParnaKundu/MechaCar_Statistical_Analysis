8edfb58863ea5e1ee77fb14de32282612e346a33
parna.kundu@gmail.com <parna.kundu@gmail.com>
2023-02-02 01:57:49 -0500

    Challenge 16 - Part 2 solution

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,16 @@ mechacar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F, stringsAsFacto
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table)) # Generate summary statistics
+
+# Part 2: Create Visualizations for the Trip Analysis
+
+# Import and Read the Suspension_Coil.csv as a dataframe
+suspension_table <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# Get the mean, median, variance, and standard deviation of the suspension coil’s PSI column
+total_summary <- suspension_table %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary
+
+# Get the mean, median, variance and standard deviation of the suspension coil's PSI column for each manufacturing lot
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+lot_summary
